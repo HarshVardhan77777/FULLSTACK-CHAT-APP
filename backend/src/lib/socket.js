5fb40226ee9cfc696fbaf0fd4a4717adcbba870d
HarshVardhan77777 <vardhanharsh8769@gmail.com>
2025-07-25 15:40:17 +0530

    changes have been made

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://fullstack-chat-app-1-a2ww.onrender.com"],
+    origin: ["http://localhost:5173"],
   },
 });
 
